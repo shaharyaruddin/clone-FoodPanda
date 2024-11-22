6f0831080ea9bc95fec6b39013632bf9f6166bf6
shaharyaruddin <sheheryark505@gmail.com>
2024-11-22 23:40:15 +0500

    working in sectionhero

---
+++ app/Sections/SectionHero/index.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+import hero from "../../../public/assets/Images/hero.jpg";
+const SectionHero = () => {
+  return (
+    <div className="flex justify-between">
+      <div className="flex justify-start items-center">
+        <p className="text-4xl font-bold">It's the food and groceries you love, delivered</p>
+      </div>
+      <div className="flex justify-end">
+        <Image 
+        src={hero}
+         alt="Hero Image"
+         width={900}
+         height={200}
+         />
+      </div>
+    </div>
+  );
+};
+
+export default SectionHero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Link from "next/link";
 import React from "react";
 import Navbar from "./Components/Navbar";
+import SectionHero from "./Sections/SectionHero";
 
 export default function page() {
   return (
     <>
-    <Navbar />
+      <Navbar />
+      <SectionHero />
     </>
   );
 }
-
