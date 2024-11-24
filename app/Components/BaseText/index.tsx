554bc05f9c8b8b4de22e803876a6915873631cd1
shaharyaruddin <sheheryark505@gmail.com>
2024-11-24 19:36:21 +0500

    added baseText interface

--- app/Components/BaseText/index.tsx
+++ app/Components/BaseText/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 
-function BaseText(
-    {
+interface BaseTextProps {
+  children: any;
+  size?: string;
+  weight?: string;
+  color?: string;
+  additionalClasses?: string;
+}
+
+function BaseText({
   children,
   size = "base",
   weight = "normal",
   color = "text-black",
   additionalClasses = "",
-}) {
+}: BaseTextProps) {
   return (
     <span className={`${size} ${weight} ${color} ${additionalClasses}`}>
       {children}
