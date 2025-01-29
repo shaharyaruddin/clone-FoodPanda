eb2c14daa784beeeb2ad34cd8b7c2afb9219f251
shaharyaruddin <sheheryark505@gmail.com>
2025-01-29 23:53:04 +0500

    added ssg

--- app/city/[cityName]/page.tsx
+++ app/city/[cityName]/page.tsx
@@ -17,9 +17,12 @@ const page = async ({ params }: any) => {
   // Fetch API data
   let data = [];
   try {
-    const response = await fetch("https://dummyjson.com/recipes", { cache: "no-store" });
+    const response = await fetch("https://dummyjson.com/recipes", {
+      cache: "force-cache", 
+
+    });
     const jsonResponse = await response.json();
-    data = jsonResponse.recipes; // Assuming the API has a `recipes` field
+    data = jsonResponse.recipes;
   } catch (error) {
     console.error("Failed to fetch recipes data:", error);
   }
@@ -32,16 +35,13 @@ const page = async ({ params }: any) => {
     </PageWrapper>
   );
 };
+;
 
 export default page;
 
-export const generateStaticParams = async () => {
-  const response = await fetch(API_URL);
-  const data = await response.json();
 
-  return data.recipes.map((item: any) => {
-    return {
-      cityName: item.id.toString(),
-    };
-  });
+export const generateStaticParams = async () => {
+  return cityList.map((item) => ({
+    cityName: item.name.toLowerCase(), 
+  }));
 };
